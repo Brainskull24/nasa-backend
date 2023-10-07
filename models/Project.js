b555d94b58607bf0b8e687185070abd610820dbd
nimit <garg.nimit36@gmail.com>
2023-10-08 01:44:57 +0530

    nimit

--- models/Project.js
+++ models/Project.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose"); 
 
 const productSchema = new mongoose.Schema ({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
   title: {
     type: String,
     required: true 
@@ -23,6 +27,10 @@ const productSchema = new mongoose.Schema ({
   skills: {
     type: String,
     required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
   photos: [
     {
