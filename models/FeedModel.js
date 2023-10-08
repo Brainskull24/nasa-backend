1d6152f1aa08b6044b43e5e6b2b69250483b87a8
nimit <garg.nimit36@gmail.com>
2023-10-08 11:03:32 +0530

    project

---
+++ models/FeedModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose"); 
+const feedSchema = new mongoose.Schema ({
+  title: {
+    type: String,
+    required: true 
+  }, 
+  summary:{
+    type:String,
+    required:true
+  }
+});
+
+module.exports = mongoose.model("Feed", feedSchema);
